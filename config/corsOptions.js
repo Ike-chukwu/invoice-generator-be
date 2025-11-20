3b18e78d0a00cd39581eda4d8d497a299a9857f1
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2025-11-20 03:14:26 +0100

    little cors fix

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const whiteList = ["http://localhost:3000"];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1) {
+    if (!origin || whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Access denied by CORS"));
