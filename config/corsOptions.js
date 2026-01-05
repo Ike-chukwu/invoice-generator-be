7c5dd7d60a415f8306f6d10d58bcfed122b44377
Ikechukwu Ugboko <103510036+Ike-chukwu@users.noreply.github.com>
2026-01-05 20:29:25 +0100

    Enhance CORS options with additional settings
    Updated CORS options to include credentials, methods, and allowed headers.

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,21 @@
-const whiteList = ["http://localhost:3000","https://invoice-fe-vlyg.vercel.app","https://invoice-fe-phi.vercel.app"];
+const whiteList = [
+  "http://localhost:3000",
+  "https://invoice-fe-vlyg.vercel.app",
+  "https://invoice-fe-phi.vercel.app",
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin ||whiteList.indexOf(origin) !== -1) {
+    if (!origin || whiteList.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Access denied by CORS"));
     }
   },
-  optionsSuccessStuatus: 200,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
-
-
-
